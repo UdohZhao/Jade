7ab54f593c217cc00425767f8eb439f857371473
puyunjun <15084341790@163.com>
2017-07-27 15:30:09 +0800

    puyunjun

--- apps/home/model/shopCart.php
+++ apps/home/model/shopCart.php
@@ -1,15 +1,19 @@
 <?php
 namespace apps\home\model;
-use use core\lib\model;
+use  core\lib\model;
 class shopCart extends model{
     public $table='shop_cart';
     /*查询购物车
     ＠param wuid 微信用户id
      * */
     public function selShopcar($wuid){
-        /*return $this->select($this->table,['[>]goods'=>['gid'=>'id']]
-                ,['shop_cart.specification','shop_cart.quantity',
-                '']);*/
+       $info = $this->select($this->table,['[>]goods'=>['gid'=>'id']]
+                ,['shop_cart.specification','shop_cart.quantity','cname',
+                'cover_path','promotion_price'],['shop_cart.wuid'=>$wuid]);
+        foreach($info as $k=>$v){
+            $info[$k]['cover_path']=unserialize($v['cover_path']);
+        }
+        return $info;
     }
 
 }
