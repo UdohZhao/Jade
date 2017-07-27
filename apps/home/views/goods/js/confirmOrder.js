ecdab55a3cdbdd82193d3fbea52de46841fd4126
yangxiaol <424310717@qq.com>
2017-07-27 23:12:42 +0800

    yangxiaoli

--- apps/home/views/goods/js/confirmOrder.js
+++ apps/home/views/goods/js/confirmOrder.js
@@ -1,61 +1,73 @@
 $(function(){
-	// var each_money=$(".each_money").html();
-	// var each_moneyInt=parseFloat(each_money).toFixed(2);
-	// console.log(each_moneyInt)
-	// var each_number=$(".each_number").html();
-	// var each_numberInt=parseInt(each_number);
-	// all_money=each_moneyInt*each_numberInt;
-	// var all_moneyInt=parseFloat(all_money).toFixed(2);
-	// $(".all_money").html(all_moneyInt);
-	// $(".allmoney_bottom").html(all_moneyInt);
-
-	// $(".jia").click(function(){
-	// 	var input_number=$("#input_number").val();
-	// 	var input_numberInt=parseInt(input_number);
-	// 	var jia=input_numberInt+1;
-	// 	$("#input_number").val(jia);
-	// 	$(".each_number").html(jia);
-	// 	$(".pro_num").html(jia);
-	// 	all_money=each_moneyInt*jia;
-	// 	var all_moneyInt=parseFloat(all_money).toFixed(2);
-	// 	$(".all_money").html(all_moneyInt);
-	// 	$(".allmoney_bottom").html(all_moneyInt);
-	// });
-	// $(".jian").click(function(){
-	// 	var input_number=$("#input_number").val();
-	// 	var input_numberInt=parseInt(input_number);
-	// 	if(input_numberInt==1){
-	// 		$("#input_number").val("1");
-	// 		$(".each_number").html("1");
-	// 		$(".pro_num").html("1");
-	// 		all_money=each_moneyInt*1;
-	// 		var all_moneyInt=parseFloat(all_money).toFixed(2);
-	// 		$(".all_money").html(all_moneyInt);
-	// 		$(".allmoney_bottom").html(all_moneyInt);
-	// 	}else{
-	// 		var jian=input_numberInt-1;
-	// 		$("#input_number").val(jian);
-	// 		$(".each_number").html(jian);
-	// 		$(".pro_num").html(jian);
-	// 		all_money=each_moneyInt*jian;
-	// 		var all_moneyInt=parseFloat(all_money).toFixed(2);
-	// 		$(".all_money").html(all_moneyInt);
-	// 		$(".allmoney_bottom").html(all_moneyInt);
-	// 	}
-	// });
-	function addMoney(){
-		var ids=$(".shopping_list")
-	    for(var i=0;i<ids.length;i++){
-          var money=$(ids[i]).parents(".shopping_left").siblings(".shopping_center").find(".each_money").html();
-          var number=$(ids[i]).parents(".shopping_left").siblings(".shopping_center").find(".each_number").html();
-          var zhengshu=Math.round(money*100);
-          var each_product=zhengshu*parseInt(number);
-          all_money+=each_product;
-	    }
-	    $('.all_money').html(all_money/100);
+	var all=0;
+	var eachall=0;
+	var all_money=0;
+	var product=$(".shopping_list");
+	for(i=0;i<product.length;i++){
+		var each=$(product[i]).find(".input_number").val();
+		var eachInt=parseInt(each);
+		all+=eachInt;
+		$(".pro_num").html(all);
+		var eachmoney=$(product[i]).find(".each_money").html();
+		var zhengshu=Math.round(eachmoney*100);
+		var eachall=zhengshu*eachInt;
+		all_money+=eachall;
+		$(".all_money").html(all_money/100);
+		$(".allmoney_bottom").html(all_money/100);
 	}
-	addMoney();
-	$(".shopping_left input").click(function(){
-	    addMoney();
-	})
+	$(".jia").click(function(){  
+		var input_number=$(this).siblings(".input_number").val();
+		var input_numberInt=parseInt(input_number);
+		var jia=input_numberInt+1;
+		$(this).siblings(".input_number").val(jia);
+		var all=0;
+		for(i=0;i<product.length;i++){
+			var each=$(product[i]).find(".input_number").val();
+			var eachInt=parseInt(each);
+			all+=eachInt;
+			$(".pro_num").html(all);
+		};
+		var eachmoney=$(this).parents(".number_chioce").siblings(".each_money").html();
+		var zhengshu=Math.round(eachmoney*100);
+		var eachall=zhengshu*eachInt;
+		all_money+=eachall;
+		$(".all_money").html(all_money/100);
+		$(".allmoney_bottom").html(all_money/100);
+	  })
+	$(".jian").click(function(){
+		var eachmoney=$(this).parents(".number_chioce").siblings(".each_money").html();
+		var zhengshu=Math.round(eachmoney*100);
+		var input_number=$(this).siblings(".input_number").val();
+		var input_numberInt=parseInt(input_number);
+		if(input_numberInt==1){
+			var all=0;
+			$(this).siblings(".input_number").val("1");
+			for(i=0;i<product.length;i++){
+				var each=$(product[i]).find(".input_number").val();
+				var eachInt=parseInt(each);
+				all+=eachInt;
+				$(".pro_num").html(all);
+				var eachall=zhengshu*eachInt;
+				all_money+=eachall;
+				console.log(eachmoney);
+				$(".all_money").html(all_money/100);
+				$(".allmoney_bottom").html(all_money/100);
+			};
+		}else{
+			var jian=input_numberInt-1;
+			var all=0;
+			$(this).siblings(".input_number").val(jian);
+			for(i=0;i<product.length;i++){
+				var each=$(product[i]).find(".input_number").val();
+				var eachInt=parseInt(each);
+				all+=eachInt;
+				$(".pro_num").html(all);
+			};
+			var zhengshu=Math.round(eachmoney*100);
+			var eachall=zhengshu*eachInt;
+			all_money+=eachall;
+			$(".all_money").html(all_money/100);
+			$(".allmoney_bottom").html(all_money/100);
+		}
+	});
 })
