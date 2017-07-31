6c3afd0a497534420898c85e40194959b41c0d8d
yangxiaol <424310717@qq.com>
2017-07-31 13:00:10 +0800

    yangxiaoli

--- apps/home/views/indent/js/detail.js
+++ apps/home/views/indent/js/detail.js
@@ -4,7 +4,7 @@ $(function(){
   var each_moneyLen=$(".each_money").length;
   for(var i=0;i<each_moneyLen;i++){
     var each_moneyHtml=$(".each_money").eq(i).html();
-    var each_moneyNum=parseFloat(each_moneyHtml);
+    var each_moneyNum=parseFloat(each_moneyHtml)*100;
     var each_moneyQuantity=$(".each_money").eq(i).siblings(".number").children("span").html();
     var each_moneyQuantity=parseFloat(each_moneyQuantity);
     each_money=each_moneyNum*each_moneyQuantity;
@@ -12,7 +12,7 @@ $(function(){
   }
   var all_moneyLen=$(".all_money").length;
   for(var i=0;i<all_moneyLen;i++){
-    $(".all_money").eq(i).html(all_money);
+    $(".all_money").eq(i).html(all_money/100);
   }
 })
 
